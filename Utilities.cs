1017b5195cf05c762ae7ae90a9492de5f4d5f82d
Fanggy-1234 <151597253+Fanggy-1234@users.noreply.github.com>
2024-02-13 09:42:31 +0700

    Add files via upload
    new upload

---
+++ Utilities.cs
@@ -0,0 +1,50 @@
+﻿using PLIMS.Models;
+using Remotion.Mixins.Definitions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services.Description;
+
+namespace PLIMS
+{
+    public class Utilities
+    {
+        public static TbUser GetCurrentUser()
+        {
+            PhlimsDatabaseEntities _db = new PhlimsDatabaseEntities();
+            TbUser user = new TbUser();
+            try
+            {
+                var username = Convert.ToInt32(HttpContext.Current.User.Identity.Name);   
+                var checkuser = _db.TbUsers.Find(username);
+               
+
+                //if (checkuser.PasswordLastUpdate.Value.AddMonths(3) >=  DateTime.Now) 
+                //{
+                    
+                //}
+                if (checkuser != null)
+                {
+                    user.UserName = checkuser.UserName;
+                    user.UserLastName = checkuser.UserLastName;
+                    user.UserEmail = checkuser.UserEmail;
+                    user.UserEmpID = checkuser.UserEmpID;
+                    user.PasswordLastUpdate = checkuser.PasswordLastUpdate;
+                    string[] linecon = checkuser.Lineconcern.Split(',');
+                    for(int i = 1;i < linecon.Length; i++) {
+                        var lineuser = _db.TbLines.Where(x => x.LineID.Equals(linecon[i]));
+                      // ค้างเขียนโปแกรม line concern ไว้
+                    }
+                   
+
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return user;
+        }
+    }
+}
